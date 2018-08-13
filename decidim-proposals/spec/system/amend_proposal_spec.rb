c3fe2a4d3c872eefab5a2249aec31fccfdcded63
Agustí B.R <agusti.br@coditramuntana.com>
2018-08-13 15:12:51 +0200

    Added Amend system Specs for Creation

--- decidim-proposals/spec/system/amend_proposal_spec.rb
+++ decidim-proposals/spec/system/amend_proposal_spec.rb
@@ -69,7 +69,6 @@ describe "Amend Proposal", type: :system do
           login_as user, scope: :user
           visit decidim.new_amend_path(amendable_gid: proposal.to_sgid.to_s)
         end
-
         it "is shown the amend title field" do
           expect(page).to have_css("#amend_title", visible: true)
         end
@@ -95,7 +94,6 @@ describe "Amend Proposal", type: :system do
           end
           click_button "Send emendation"
         end
-
         it "is shown the Success Callout" do
           expect(page).to have_css(".callout.success", text: "The amendment has been created successfully")
         end
